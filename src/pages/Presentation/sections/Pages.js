4a0cce3f62ffe85eb5a552ae184888b326649ebf
MohamedArshadPerpex <arshad.perpex@gmail.com>
2024-07-18 14:22:29 +0530

    changes

--- src/pages/Presentation/sections/Pages.js
+++ src/pages/Presentation/sections/Pages.js
@@ -13,78 +13,88 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-// react-router-dom components
-import { Link } from "react-router-dom";
-
 // @mui material components
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
-import MKBadge from "components/MKBadge";
-import MKTypography from "components/MKTypography";
 
-// Presentation page components
-import ExampleCard from "pages/Presentation/components/ExampleCard";
+// Material Kit 2 React examples
+import RotatingCard from "examples/Cards/RotatingCard";
+import RotatingCardFront from "examples/Cards/RotatingCard/RotatingCardFront";
+import RotatingCardBack from "examples/Cards/RotatingCard/RotatingCardBack";
+import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
 
-// Data
-import data from "pages/Presentation/sections/data/pagesData";
+// Images
+import bgFront from "assets/images/rotating-card-bg-front.jpeg";
+import bgBack from "assets/images/rotating-card-bg-back.jpeg";
 
 function Pages() {
-  const renderData = data.map(({ image, name, route }) => (
-    <Grid item xs={12} md={6} sx={{ mb: { xs: 3, lg: 0 } }} key={name}>
-      <Link to={route}>
-        <ExampleCard image={image} name={name} display="grid" minHeight="auto" />
-      </Link>
-    </Grid>
-  ));
-
   return (
-    <MKBox component="section" py={6}>
+    <MKBox component="section" py={6} my={6}>
       <Container>
-        <Grid
-          container
-          item
-          xs={12}
-          lg={6}
-          flexDirection="column"
-          alignItems="center"
-          sx={{ textAlign: "center", my: 6, mx: "auto", px: 0.75 }}
-        >
-          <MKBadge
-            variant="contained"
-            color="info"
-            badgeContent="boost creativity"
-            container
-            sx={{ mb: 2 }}
-          />
-          <MKTypography variant="h2" fontWeight="bold">
-            With our coded pages
-          </MKTypography>
-          <MKTypography variant="body1" color="text">
-            The easiest way to get started is to use one of our
-            <br /> pre-built example pages.
-          </MKTypography>
-        </Grid>
-      </Container>
-      <Container sx={{ mt: { xs: 8, lg: 16 } }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} lg={9} sx={{ mt: 3, px: { xs: 0, lg: 8 } }}>
-            <Grid container spacing={3}>
-              {renderData}
-            </Grid>
+        <Grid container item xs={11} spacing={3} alignItems="center" sx={{ mx: "auto" }}>
+          <Grid item xs={12} lg={4} sx={{ mx: "auto" }}>
+            <RotatingCard>
+              <RotatingCardFront
+                image={bgFront}
+                icon="touch_app"
+                title={
+                  <>
+                    Feel the
+                    <br />
+                    Material Kit
+                  </>
+                }
+                description="All the MUI components that you need in a development have been re-design with the new look."
+              />
+              <RotatingCardBack
+                image={bgBack}
+                title="Discover More"
+                description="You will save a lot of time going from prototyping to full-functional code because all elements are implemented."
+                action={{
+                  type: "internal",
+                  route: "/sections/page-sections/page-headers",
+                  label: "start with header",
+                }}
+              />
+            </RotatingCard>
           </Grid>
-          <Grid item xs={12} lg={3}>
-            <MKBox position="sticky" top="100px" pb={{ xs: 2, lg: 6 }}>
-              <MKTypography variant="h3" fontWeight="bold" mb={1}>
-                Presentation Pages for Company, Landing Pages, Blogs and Support
-              </MKTypography>
-              <MKTypography variant="body2" fontWeight="regular" color="secondary" mb={1} pr={2}>
-                These is just a small selection of the multiple possibitilies you have. Focus on the
-                business, not on the design.
-              </MKTypography>
-            </MKBox>
+          <Grid item xs={12} lg={7} sx={{ ml: "auto" }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="content_copy"
+                  title="Full Documentation"
+                  description="Built by developers for developers. Check the foundation and you will find
+                    everything inside our documentation."
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="flip_to_front"
+                  title="MUI Ready"
+                  description="The world's most popular react components library for building user interfaces."
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={3} sx={{ mt: { xs: 0, md: 6 } }}>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="price_change"
+                  title="Save Time & Money"
+                  description="Creating your design from scratch with dedicated designers can be very expensive. Start with our Design System."
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="devices"
+                  title="Fully Responsive"
+                  description="Regardless of the screen size, the website content will naturally fit the given resolution."
+                />
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
